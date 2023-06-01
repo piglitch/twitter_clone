b459366b94840666f271d5ba3f22b08b3cb7957b
Avi <avibanerjee2000@gmail.com>
2023-06-02 00:15:17 +0530

    .

--- twitter-clone/src/Sidebar.js
+++ twitter-clone/src/Sidebar.js
@@ -5,7 +5,10 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';import MailOutlineIcon from '@mui/icons-material/MailOutline';
-
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function Sidebar() {
   return (
@@ -13,11 +16,15 @@ function Sidebar() {
         {/* Twitter icon */}
         <TwitterIcon />
 
-        <SidebarOption Icon={HomeIcon} text={"Home"}/>
+        <SidebarOption active Icon={HomeIcon} text={"Home"}/>
         <SidebarOption Icon={SearchIcon} text={"Explore"}/>
         <SidebarOption Icon={NotificationsNoneIcon} text={"Notifications"}/>        
         <SidebarOption Icon={MailOutlineIcon} text={"Messages"}/>
-        
+        <SidebarOption Icon={BookmarkBorderIcon} text={"Bookmarks"}/>
+        <SidebarOption Icon={ListAltIcon} text={"Lists"}/>
+        <SidebarOption Icon={PermIdentityIcon} text={"Profile"}/>
+        <SidebarOption Icon={MoreHorizIcon} text={"More"}/>
+
 
         {/* sidebarOption */}
         {/* sidebarOption */}
